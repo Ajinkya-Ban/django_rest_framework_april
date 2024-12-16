eb33a5dddeaa827a303bcdf4549276ed0fe374ec
Ajinkya-Ban <ajinkyaban@gmail.com>
2024-12-16 17:43:01 +0530

    update delete api deployed

--- restdemo1/myapp/urls.py
+++ restdemo1/myapp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProdcutListView,ProductCreateView
+from .views import ProdcutListView,ProductCreateView,ProductUpdateView,ProdcutDeleteView
 
 urlpatterns = [
 
     path('items/', ProdcutListView.as_view(), name="plist"),
-    path('create/',ProductCreateView.as_view(), name="p-create")
+    path('create/',ProductCreateView.as_view(), name="p-create"),
+    path('update-prod/<int:pk>',ProductUpdateView.as_view(),name="update-prod"),
+    path('delete-prod/<int:pk>',ProdcutDeleteView.as_view(), name="delete-prod")
 ]
--- restdemo1/myapp/views.py
+++ restdemo1/myapp/views.py
@@ -12,13 +12,34 @@ class ProdcutListView(APIView):
         serializer = ProductSerializer(items, many=True)
         return Response(serializer.data)
 
+
 class ProductCreateView(APIView):
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ProductUpdateView(APIView):
+    def put(self, request, pk):
+        try:
+            items = Product.objects.get(pk=pk)
+        except items.DoesNotExist:
+            raise Response({"error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(items, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ProdcutDeleteView(APIView):
+    def delete(self, request, pk):
+        try:
+            items = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
+        items.delete()
+        return Response({"message": "Data deleted..."}, status=status.HTTP_200_OK)
